9c7c99e7c8b368c9e8f6c2faaeb2d5876db3b0a7
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2024-12-03 14:13:55 +0900

    Include Apache Maven

---
+++ apache-maven-test/src/main/java/com/learning/java/App.java
@@ -0,0 +1,10 @@
+package com.learning.java;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Apache Maven Project!");
+    }
+}
