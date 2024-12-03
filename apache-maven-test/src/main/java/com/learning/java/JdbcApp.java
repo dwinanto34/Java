e9c9e95026a1eff4de502ed02cca4676170ba8b9
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2024-12-03 15:30:09 +0900

    JDBC execute statement

--- apache-maven-test/src/main/java/com/learning/java/JdbcApp.java
+++ apache-maven-test/src/main/java/com/learning/java/JdbcApp.java
@@ -2,8 +2,12 @@ package com.learning.java;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class JdbcApp {
     // HikariCP -- Connection pool
@@ -34,6 +38,61 @@ public class JdbcApp {
         return hikariDataSource.getConnection();
     }
 
+    private static void executeCreateTable(Connection conn) throws Exception {
+        String query = "CREATE TABLE IF NOT EXISTS public.general_products (" +
+                "id CHARACTER VARYING(36), " +
+                "name CHARACTER VARYING(100), " +
+                "description TEXT, " +
+                "price NUMERIC(19, 2)" +
+                ");";
+        try (Statement stmt = conn.createStatement()) {
+            int affected = stmt.executeUpdate(query);
+            System.out.println("Create table: " + affected);
+        }
+    }
+
+    private static void executeInsertData(Connection conn) throws Exception {
+        String query = "INSERT INTO general_products(id, name, description, price) " +
+                "VALUES " +
+                "('0001', 'iPhone 1', 'this is iPhone 1', 1000), " +
+                "('0002', 'iPhone 2', 'this is iPhone 2', 1000), " +
+                "('0003', 'iPhone 3', 'this is iPhone 3', 1000);";
+        try (Statement stmt = conn.createStatement()) {
+            int affected = stmt.executeUpdate(query);
+            System.out.println("Insert data: " + affected);
+        }
+    }
+
+    private static void executeGetData(Connection conn) throws Exception {
+        String query = "SELECT * FROM general_products";
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            System.out.println("Get data");
+
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+                String description = rs.getString("description");
+                BigDecimal price = rs.getBigDecimal("price");
+                StringBuilder stringBuilder = new StringBuilder()
+                        .append("ID: " + id)
+                        .append(" Name: " + name)
+                        .append(" Description: " + description)
+                        .append(" Rating: " + price);
+                System.out.println(stringBuilder);
+            }
+        }
+    }
+
+    private static void executeDropTable(Connection conn) throws Exception {
+        String query = "DROP TABLE IF EXISTS public.general_products;";
+        try (Statement stmt = conn.createStatement()) {
+            int affected = stmt.executeUpdate(query);
+            System.out.println("Drop table: " + affected);
+        }
+    }
+
     private static Connection getConnection() throws Exception {
         String url = "jdbc:postgresql://localhost:5432/product";
         String username = "postgres";
@@ -47,6 +106,11 @@ public class JdbcApp {
         // try-with-resource will handle close connection automatically
         try (Connection conn = getHikariConnection()) {
             System.out.println("Connected to the PostgreSQL server successfully.");
+
+            executeCreateTable(conn);
+            executeInsertData(conn);
+            executeGetData(conn);
+            executeDropTable(conn);
         } catch (Exception e) {
             e.printStackTrace();
         }
