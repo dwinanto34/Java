cd6ec247284ef4d059d14ca848d82ef01245b0ad
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2024-12-03 15:05:18 +0900

    JDBC connection pool using HikariCP

--- apache-maven-test/src/main/java/com/learning/java/JdbcApp.java
+++ apache-maven-test/src/main/java/com/learning/java/JdbcApp.java
@@ -1,9 +1,39 @@
 package com.learning.java;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JdbcApp {
+    // HikariCP -- Connection pool
+    // Creating a new database connection for each request is expensive and can lead to delays.
+    // Connection pooling reuses existing connections from the pool, making it much faster and more efficient.
+    // Connection pools limit the number of active connections, preventing database overloading.
+    // It ensures that connections are properly reused without reaching the database server's maximum connections.
+    private static HikariDataSource hikariDataSource;
+
+    static {
+        // Configure HikariCP
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/product");
+        config.setUsername("postgres");
+        config.setPassword("admin");
+        config.setMaximumPoolSize(10);
+
+        config.setMinimumIdle(2);
+        config.setIdleTimeout(30 * 1_000); // 30 seconds
+        config.setMaxLifetime(30 * 60 * 1_000); // 30 minutes
+        config.setConnectionTimeout(30 * 1_000); // 30 seconds
+
+        // Initialize the data source
+        hikariDataSource = new HikariDataSource(config);
+    }
+
+    private static Connection getHikariConnection() throws Exception {
+        return hikariDataSource.getConnection();
+    }
+
     private static Connection getConnection() throws Exception {
         String url = "jdbc:postgresql://localhost:5432/product";
         String username = "postgres";
@@ -15,10 +45,11 @@ public class JdbcApp {
     public static void run() {
         // JDBC Connection
         // try-with-resource will handle close connection automatically
-        try (Connection conn = getConnection()) {
+        try (Connection conn = getHikariConnection()) {
             System.out.println("Connected to the PostgreSQL server successfully.");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
